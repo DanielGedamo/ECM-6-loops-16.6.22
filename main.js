78a1a85fc0668d62b0be13ead910a949913f6af4
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-21 00:25:42 +0300

    1

---
+++ main.js
@@ -0,0 +1,92 @@
+// 1
+// let claas = document.getElementsByClassName("myClass");
+//  for(const i of claas){
+//     console.log(i);
+// }
+// 2
+// function changeColor() {
+//   let eli =document.getElementsByClassName("mybbb");
+//   for (const i of eli) {
+//     i.style.color= "blue";
+//   }
+// }
+// function changetext(){
+//     let daniel = document.getElementsByClassName("mybbb")
+//     for (const myiterator of daniel){
+//         myiterator.value = "hello" 
+//     }
+// }
+ 
+// let myOBJ ={
+//     firstName:"daniel",
+//     lastName :"gedamo",
+//     age:23,
+//     mail:"dani@gamil.com"
+// }
+// for (const key in myOBJ) {
+//  console.log(key + " " +myOBJ[key]);
+// }
+// for(const key in myOBJ){
+//     console.log(myOBJ[key]);
+// }
+// function printObject(){
+//    let first = document.getElementById("first_name").value;
+//    let last_Name = document.getElementById("last_name").value;
+//    let age = document.getElementById("userAge").value;
+//     let myOBJ ={
+//         first_Name : first,
+//         last_Name: last_Name,
+//         age: age
+//     }
+//     for(const key in myOBJ){
+//         document.write(key + ": " + myOBJ[key]+" " )
+//     }
+// }
+let myArray =[
+    {
+       firstName: CD
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let obj1 = {
+//     age: "22",
+//     first : "daniel",
+//     lastName : "gedamo",
+//  }
+//  let obj2 = {
+//     age: "13",
+//     first : "yosi",
+//     lastName : "balacho",
+//  }
+//  let myArray = [obj1,obj2 ]
+//  function getDitalsAndPrint (nmm){
+// myArray.forEach(item => {
+//     if (item.age >18){
+//         document.write(item.first +"<br>")
+//     }
+//     else (document.write(item.lastName));
+// });
+// }getDitalsAndPrint(myArray)
